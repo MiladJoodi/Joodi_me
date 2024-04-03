f9cf45612ed5296b61cac699e81c8238398dd492
Milad Joodi <winbeta.com@gmail.com>
2024-04-03 22:31:02 +0330

    up

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
       title: 'Joodi',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo.png',
       },
       items: [
         {
