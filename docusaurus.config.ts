e88fdd6306ee099005dcd10aad7b15efa739e3e2
Milad Joodi <winbeta.com@gmail.com>
2024-04-07 13:29:41 +0330

    up

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,6 +72,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {to: '/about', label: 'About', position: 'left'},
+        {to: '/projects', label: 'Projects', position: 'left'},
         {
           href: 'https://www.linkedin.com/in/MiladJoodi',
           label: 'Linkedin',
